695fd1816051c9786efcbdb72e801f99166d569a
Steve Kinney <hello@stevekinney.net>
2018-03-10 16:44:37 -0700

    Get like halfway through using setState

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,65 @@
 import React, { Component } from 'react';
+import cn from 'classnames';
 
 import './Card.css';
 
 export default class Card extends Component {
-  element = null;
+  state = {
+    isDragging: false,
+    offset: null,
+  };
 
-  render () {
+  element = null;
+  startingX = null;
+  currentX = null;
+
+  onMove(event) {
+    this.currentX = event.pageX || event.touches.pageX[0];
+  }
+
+  handleMouseDown = (event) => {
+    console.log(event.pageX);
+
+    this.startingX = event.pageX;
+
+    this.setState({
+      isDragging: true,
+    });
+  }
+
+  handleMouseMove = (event) => {
+    const { isDragging } = this.state;
+    if (!isDragging) return;
+
+    const offset = event.pageX - this.startingX;
+    this.setState({ offset });
+  };
+
+  handleMouseUp = () => {
+    this.setState({
+      isDragging: false,
+      offset: 0,
+    });
+  };
+
+  render() {
     const { label } = this.props;
+    const { isDragging, offset } = this.state;
     return (
-      <div class="Card" ref={(element) => { this.element = element; }}>
+      <div
+        className={
+          cn('Card', {
+            'Card--is-dragging': isDragging,
+          })
+        }
+        style={{
+          willChange: isDragging ? 'transform' : 'auto',
+          transform: `translateX(${offset || 0}px)`,
+        }}
+        onMouseDown={this.handleMouseDown}
+        onMouseMove={this.handleMouseMove}
+        onMouseUp={this.handleMouseUp}
+      >
         {label}
       </div>
     );
