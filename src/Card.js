07d3a297360157131ce213441a3565b7546b1641
Steve Kinney <hello@stevekinney.net>
2018-03-10 17:41:14 -0700

    Basic implementation

--- src/Card.js
+++ src/Card.js
@@ -1,64 +1,65 @@
 import React, { Component } from 'react';
-import cn from 'classnames';
 
 import './Card.css';
 
 export default class Card extends Component {
-  state = {
-    isDragging: false,
-    offset: null,
-  };
-
   element = null;
   startingX = null;
-  currentX = null;
 
-  onMove(event) {
-    this.currentX = event.pageX || event.touches.pageX[0];
-  }
-
-  handleMouseDown = (event) => {
-    console.log(event.pageX);
-
-    this.startingX = event.pageX;
-
-    this.setState({
-      isDragging: true,
-    });
-  }
-
-  handleMouseMove = (event) => {
-    const { isDragging } = this.state;
-    if (!isDragging) return;
-
-    const offset = event.pageX - this.startingX;
-    this.setState({ offset });
+  handleMouseEnter = () => {
+    this.element.style.willChange = 'transform';
   };
 
-  handleMouseUp = () => {
-    this.setState({
-      isDragging: false,
-      offset: 0,
-    });
+  handleMouseLeave = () => {
+    if (this.isDragging) return;
+    this.element.style.willChange = '';
+  };
+
+  handleStart = (event) => {
+    this.startingX = event.pageX || (event.touches && event.touches[0].pageX);
+    this.isDragging = true;
+    document.addEventListener('mousemove', this.handleMove);
+    document.addEventListener('mouseup', this.handleEnd);
+  }
+
+  handleMove = (event) => {
+    event.preventDefault();
+    const currentX = event.pageX || (event.touches && event.touches[0].pageX);
+    if (!currentX) return;
+    const offset = currentX - this.startingX;
+    this.element.style.transform = `translateX(${offset}px)`;
+  };
+
+  handleEnd = () => {
+
+    this.isDragging = false;
+    this.element.style.transform = 'auto';
+
+    document.removeEventListener('mousemove', this.handleMove);
+    document.removeEventListener('mouseup', this.handleMouseUp);
+
+    this.element.addEventListener('transitionend', this.handleTransitionEnd);
+    this.element.style.transition = `transform 0.2s`;
+    this.element.style.transform = '';
+
+  };
+
+  handleTransitionEnd = () => {
+    console.log('transitionend');
+    this.element.style.transition = '';
+    this.element.style.willChange = '';
+    this.element.removeEventListener('transitionend', this.handleTransitionEnd);
   };
 
   render() {
     const { label } = this.props;
-    const { isDragging, offset } = this.state;
     return (
       <div
-        className={
-          cn('Card', {
-            'Card--is-dragging': isDragging,
-          })
-        }
-        style={{
-          willChange: isDragging ? 'transform' : 'auto',
-          transform: `translateX(${offset || 0}px)`,
-        }}
-        onMouseDown={this.handleMouseDown}
-        onMouseMove={this.handleMouseMove}
-        onMouseUp={this.handleMouseUp}
+        className="Card"
+        ref={element => this.element = element}
+        onMouseDown={this.handleStart}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
       >
         {label}
       </div>
